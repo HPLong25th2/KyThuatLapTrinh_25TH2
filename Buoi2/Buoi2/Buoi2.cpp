62bc4723680ab02c9219db8b68d39ab8d029ae43
Admin <Admin@ADMIN-PC>
2026-04-02 10:45:43 +0700

    xong buoi 2

--- Buoi2/Buoi2/Buoi2.cpp
+++ Buoi2/Buoi2/Buoi2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 using namespace std;
 
 struct Person {
@@ -20,6 +21,14 @@ struct Person {
         getline(in, p.hometown);
         return in;
     }
+    friend ostream& operator<<(ostream& out, const Person& p) {
+        out << "Person information: " << endl;
+        out << "\t+ Id: " << p.id << endl;
+        out << "\t+ Name: " << p.name << endl;
+        out << "\t+ Age: " << p.age << endl;
+        out << "\t+ Home town: " << p.hometown << endl;
+        return out;
+	}
 
 };  
 
@@ -28,16 +37,76 @@ void Show(vector<Person>p) {
         cout << "An empty list" << endl;
     else {
         for (int i = p.size() - 1; i >= 0; i--) {
-            cout << "\t + Id: " << p[i].id << endl;
-            cout << "\t + name: " << p[i].name << endl;
-            cout << "\t + age: " << p[i].age << endl;
-            cout << "\t + Home town: " << p[i].hometown << endl;
+			cout << "Person information: " << endl;
+            cout << "\t+ Id: " << p[i].id << endl;
+            cout << "\t+ Name: " << p[i].name << endl;
+            cout << "\t+ Age: " << p[i].age << endl;
+			cout << "\t+ Home town: " << p[i].hometown << endl;
         }
     }
+}
+bool Remove(vector<Person>& p, int id) {
+    for (int i = 0; i < p.size(); i++) {
+        if (p[i].id == id) {
+            p.erase(p.begin() + i);
+            return true;
+        }
+    }
+    return false;
+}
+void Find(vector<Person> p, string name) {
+	bool found = false; 
+    for (int i = p.size() - 1; i >= 0; i--) {
+        if (p[i].name == name) {
+			found = true;
+			cout << p[i];
+        }
+    }
+	if (!found) 
+		cout << "No person is found with name: " << name << endl;
+}
+void Export(vector<Person> p, string filename) {
+    ofstream out(filename);
+    if (!out.is_open()) {
+        cout << "Can't open file: " << filename << endl;
+        return;
+    }
+    for (int i = 0; i < p.size(); i++) {
+        out << p[i].id << endl;
+        out << p[i].name << endl;
+        out << p[i].age << endl;
+        out << p[i].hometown << endl;
+    }
+    out.close();
+	cout << "Export successful to file: " << filename << endl;
+}
+void Import(vector<Person>& p, string filename) {
+    ifstream in(filename);
+    if (!in.is_open()) {
+        cout << "Can't open file: " << filename << endl;
+        return;
+    }
+    p.clear();
+    while (!in.eof()) {
+        Person a;
+		a.id = 0;
+		in >> a.id;
+        in.ignore();
+        getline(in, a.name);
+        in >> a.age;
+        in.ignore();
+        getline(in, a.hometown);
+        if (a.id == 0)
+            break;
+            p.push_back(a);
+    }
+    in.close(); 
+    cout << "Import successful from file: " << filename << endl;
 }
 int main() {
 	vector<Person> list;
     do {
+		system("cls");
         cout << "---- HUMAN RESOURCE MANAGEMENT ----" << endl;
         cout << "1. Show person list" << endl;
         cout << "2. Add a person" << endl;
@@ -45,6 +114,7 @@ int main() {
         cout << "4. Find people by name" << endl;
         cout << "5. Export to file" << endl;
         cout << "6. Import from file" << endl;
+		cout << "0. Quit" << endl;
         cout << "_______________________" << endl;
         cout << "your command: ";
         int cmd;
@@ -56,31 +126,41 @@ int main() {
         }
         case 2: {
             Person a;
-            cout << "Id: ";
-            cin >> a.id;
-            cout << "Name: ";
-            cin >> a.name;
-            cout << "Age: ";
-            cin >> a.age;
-            cout << "Home town: ";
-            cin >> a.hometown;
-
+			cin >> a;
+            list.push_back(a);
             break;
         }
         case 3: {
+			int removeId;
+            cout << "Enter ID to remove: ";
+            cin >> removeId;
+			bool res = Remove(list, removeId);
+			if (res) {
+				cout << "   Remove successful." << endl;
+			}
+			else {
+				cout << "Id not found try again." << endl;
+			}
             break;
         }
         case 4: {
+			string name;
+			cout << "Enter name to find: ";
+			cin.ignore();
+			getline(cin, name);
+			Find(list, name);
             break;
         }
         case 5: {
+			Export(list, "25TH2.dla");  
             break;
         }
         case 6: {
+            Import(list, "25TH2.dla");
             break;
         }
         case 0: {
-            return;
+            return 0;
         }
         default: {
             cout << "your command isn`t found, try again" << endl;
