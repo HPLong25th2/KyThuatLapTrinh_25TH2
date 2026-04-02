21bd07cd8561f5268c0a2a1fd3eb9463ac338ae7
Admin <Admin@ADMIN-PC>
2026-04-02 09:00:43 +0700

    xong buoi 2

---
+++ Buoi2/Buoi2/Buoi2.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct Person {
+    int id, age;
+    string name, hometown;
+    friend istream& operator>>(istream& in, Person& p) {
+        cout << "enter person information: " << endl;
+        cout << "\t+ Id: ";
+        in >> p.id;
+        cout << "\t+ Name: ";
+        in.ignore();
+        getline(in, p.name);    
+        cout << "\t+ Age: ";
+        in >> p.age;
+        cout << "\t+ Home town: ";
+        in.ignore();
+        getline(in, p.hometown);
+        return in;
+    }
+
+};  
+
+void Show(vector<Person>p) {
+    if (p.size() == 0)
+        cout << "An empty list" << endl;
+    else {
+        for (int i = p.size() - 1; i >= 0; i--) {
+            cout << "\t + Id: " << p[i].id << endl;
+            cout << "\t + name: " << p[i].name << endl;
+            cout << "\t + age: " << p[i].age << endl;
+            cout << "\t + Home town: " << p[i].hometown << endl;
+        }
+    }
+}
+int main() {
+	vector<Person> list;
+    do {
+        cout << "---- HUMAN RESOURCE MANAGEMENT ----" << endl;
+        cout << "1. Show person list" << endl;
+        cout << "2. Add a person" << endl;
+        cout << "3. Remove a person by id" << endl;
+        cout << "4. Find people by name" << endl;
+        cout << "5. Export to file" << endl;
+        cout << "6. Import from file" << endl;
+        cout << "_______________________" << endl;
+        cout << "your command: ";
+        int cmd;
+        cin >> cmd;
+        switch (cmd) {
+        case 1: {
+            Show(list);
+            break;
+        }
+        case 2: {
+            Person a;
+            cout << "Id: ";
+            cin >> a.id;
+            cout << "Name: ";
+            cin >> a.name;
+            cout << "Age: ";
+            cin >> a.age;
+            cout << "Home town: ";
+            cin >> a.hometown;
+
+            break;
+        }
+        case 3: {
+            break;
+        }
+        case 4: {
+            break;
+        }
+        case 5: {
+            break;
+        }
+        case 6: {
+            break;
+        }
+        case 0: {
+            return;
+        }
+        default: {
+            cout << "your command isn`t found, try again" << endl;
+        }
+        }
+        cout << " press Enter to continue... ";
+        cin.ignore();
+        cin.get();
+    } while (true);
+}
